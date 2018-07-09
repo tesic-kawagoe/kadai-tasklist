9f7426ee6b2dcaf8ddd01a443f88954d9a5d38ce
tesic-kawagoe <con@tes-ic.com>
2018-07-09 21:45:57 +0000

    tasklist

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -19,11 +19,10 @@
         </tr>
     </table>
     
-    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]), ['class' => 'btn btn-default']) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
     
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-default']) !!}
     {!! Form::close() !!}
-
-
+    
 @endsection
