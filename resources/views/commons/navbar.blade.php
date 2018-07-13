be7752d8db46353ddf019f2d896abb86356a3ade
tesic-kawagoe <con@tes-ic.com>
2018-07-13 04:34:16 +0000

    top page

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,11 +8,12 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">TaskList</a>
+                <a class="navbar-brand" href="/">Microposts</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>{!! link_to_route('tasks.create', '新規タスク作成ページ') !!}</li>
+                    <li><a href="#">Signup</a></li>
+                    <li><a href="#">Login</a></li>
                 </ul>
             </div>
         </div>
