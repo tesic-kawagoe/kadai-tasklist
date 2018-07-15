e665e4b8396ed941c518196f19435f6e2677ae45
tesic-kawagoe <con@tes-ic.com>
2018-07-15 08:48:59 +0000

    tasklist

--- database/migrations/2018_07_15_150108_create_microposts_table.php
+++ database/migrations/2018_07_15_150108_create_microposts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
+class CreateMicropostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,20 +13,17 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('microposts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('content');
+            $table->integer('user_id')->unsigned()->index();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('microposts');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'MicropostsController@index');
+
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
-Route::get('/', 'TasksController@index');
+Route::get('/', 'MicropostsController@index');
 Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', 'TasksController@index');
     Route::resource('tasks', 'TasksController');
-    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
